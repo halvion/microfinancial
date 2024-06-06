d06d3cf093c04837274c1011788a922ef79a2287
halvion <m4twill0@gmail.com>
2024-06-06 08:09:19 +0700

    Added new assets & fixed database

--- microfinancial/version_2/packages/expense_repository/lib/src/firebase_expense_repo.dart
+++ microfinancial/version_2/packages/expense_repository/lib/src/firebase_expense_repo.dart
@@ -33,15 +33,29 @@ class FirebaseExpenseRepo implements ExpenseRepository {
   }
   
   @override
-  Future<void> createExpense(Expense expense) {
-    // TODO: implement createExpense
-    throw UnimplementedError();
+  Future<void> createExpense(Expense expense) async {
+    try {
+      await expenseCollection
+        .doc(expense.expenseId)
+        .set(expense.toEntity().toDocument());
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
   }
-  
+
   @override
-  Future<List<Expense>> getExpenses() {
-    // TODO: implement getExpenses
-    throw UnimplementedError();
+  Future<List<Expense>> getExpenses() async {
+    try {
+      return await expenseCollection
+        .get()
+        .then((value) => value.docs.map((e) => 
+          Expense.fromEntity(ExpenseEntity.fromDocument(e.data()))
+        ).toList());
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
   }
 
 }
